075ed707b3579805f557f2dd794c392f641aaf48
lianyz <lianyz@email.cn>
2022-06-13 19:14:59 +0800

    uts

--- demos/uts.go
+++ demos/uts.go
@@ -16,7 +16,12 @@ import (
 func main() {
 	cmd := exec.Command("sh")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS |
+			syscall.CLONE_NEWIPC |
+			syscall.CLONE_NEWPID |
+			syscall.CLONE_NEWNS |
+			syscall.CLONE_NEWUSER |
+			syscall.CLONE_NEWNET,
 	}
 
 	cmd.Stdin = os.Stdin
