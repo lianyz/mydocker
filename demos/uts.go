51a9d2447ae13c40e9e095f7d87e906caa4284f5
lianyz <lianyz@email.cn>
2022-06-13 18:53:39 +0800

    uts

---
+++ demos/uts.go
@@ -0,0 +1,29 @@
+/*
+@Time : 2022/6/13 18:38
+@Author : lianyz
+@Description :
+*/
+
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+/*
+@Time : 2022/6/13 18:49 
+@Author : lianyz
+@Description : 
+*/
+
+package mydocker
