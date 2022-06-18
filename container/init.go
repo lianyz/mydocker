b04cd75103cfad3e7d34aa4cfff204575aa53afc
lianyz <lianyz@email.cn>
2022-06-18 16:43:40 +0800

    mnt

--- container/init.go
+++ container/init.go
@@ -11,6 +11,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
 	"syscall"
 )
@@ -58,9 +59,14 @@ func readUserCommand() []string {
 }
 
 func setUpMount() error {
+	err := pivotRoot()
+	if err != nil {
+		logrus.Errorf("pivot root, err: %v", err)
+		return err
+	}
 	// systemd加入linux后，mount namespace就变成shared by default，
 	// 所以必须显式声明要这个新的mount namespace独立
-	err := syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, "")
+	err = syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, "")
 	if err != nil {
 		return err
 	}
@@ -74,4 +80,53 @@ func setUpMount() error {
 	}
 
 	return nil
+}
+
+// 改变当前root的文件系统
+func pivotRoot() error {
+	root, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	logrus.Infof("current location is %s", root)
+
+	err = syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, "")
+	if err != nil {
+		return err
+	}
+
+	// 为了使当前root的老root和新root不在同一个文件系统下，我们把root重新mount了一次
+	// bind mount是把相同的内容换了一个挂载点的挂载方法
+	err = syscall.Mount(root, root, "bind", syscall.MS_BIND|syscall.MS_REC, "")
+	if err != nil {
+		return fmt.Errorf("mount rootfs to itself error: %v", err)
+	}
+
+	// 创建rootfs/.pivot_root 存储 old_root
+	pivotDir := filepath.Join(root, ".pivot_root")
+	_, err = os.Stat(pivotDir)
+	if err != nil && os.IsNotExist(err) {
+		if err := os.Mkdir(pivotDir, 0777); err != nil {
+			return err
+		}
+	}
+
+	// pivot_root 到新的rootfs，现在老的 old_root 是挂载在rootfs/.pivot_root
+	// 挂载点现在依然可以在mount命令中看到
+	if err := syscall.PivotRoot(root, pivotDir); err != nil {
+		return fmt.Errorf("pivot_root %v", err)
+	}
+
+	// 修改当前的工作目录到根目录
+	if err := syscall.Chdir("/"); err != nil {
+		return fmt.Errorf("chdir / %v", err)
+	}
+
+	pivotDir = filepath.Join("/", ".pivot_root")
+	if err := syscall.Unmount(pivotDir, syscall.MNT_DETACH); err != nil {
+		return fmt.Errorf("unmount pivot_root dir %v", err)
+	}
+
+	// 删除临时文件夹
+	return os.Remove(pivotDir)
 }
