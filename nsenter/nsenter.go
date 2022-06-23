3a5ce0b6b8831df58793184fe3e9e641eeb3dc93
lianyz <lianyz@email.cn>
2022-06-23 12:14:46 +0800

    cgo

--- nsenter/nsenter.go
+++ nsenter/nsenter.go
@@ -20,11 +20,37 @@ __attribute__((constructor)) void enter_namespace(void) {
     char *docker_pid;
     docker_pid = getenv("docker_pid");
     if (docker_pid) {
-        fprintf(stdout, "******get docker_pid=%s\n", docker_pid);
+        fprintf(stdout, "[cgo]: get docker_pid=%s\n", docker_pid);
     } else {
-        fprintf(stdout, "******missing docker_pid env skip nsenter\n");
+        fprintf(stdout, "[cgo]: missing docker_pid env skip nsenter\n");
         return;
     }
+    char *docker_cmd;
+    docker_cmd = getenv("docker_cmd");
+    if (docker_cmd) {
+        fprintf(stdout, "[cgo]: get docker_cmd=%s\n", docker_cmd);
+    } else {
+        fprintf(stdout, "[cgo]: mising docker_cmd env skip nsenter\n");
+        return;
+    }
+
+    int i;
+    char nspath[1024];
+    char *namespaces[] = { "ipc", "uts", "net", "pid", "mnt" };
+    for (i = 0; i < 5; i++) {
+        sprintf(nspath, "/proc/%s/ns/%s", docker_pid, namespaces[i]);
+        int fd = open(nspath, O_RDONLY);
+        // 调用setns系统调用，进入对应的namespace
+        if (setns(fd, 0) == -1) {
+            fprintf(stdout, "[cgo]: setns on %s namespace failed: %s\n", namespaces[i], strerror(errno));
+        } else {
+            fprintf(stdout, "[cgo]: setns on %s namespace succeeded\n", namespaces[i]);
+        }
+        close(fd);
+    }
+    int res = system(docker_cmd);
+    exit(0);
+    return;
 }
 */
 import "C"
