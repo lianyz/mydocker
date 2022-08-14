5dc35063765015444b9e7482ec5aded01f9530a5
lianyz <lianyz@email.cn>
2022-08-14 21:14:17 +0800

    add images

---
+++ cleanup.sh
@@ -0,0 +1,13 @@
+rm -rf /root/alpine
+rm -rf /root/busybox
+rm -rf /root/redis
+rm -rf /root/myalpine
+rm -rf /root/mynginx
+rm -rf /root/myredis
+rm -rf /root/myflask
+
+rm -rf /root/mnt
+rm -rf /root/writeLayer
+rm -rf /root/work
+
+rm -rf /var/run/mydocker
